960e578f0805158215873e3e967493f9f1ae7ce2
a114221056-maker <a114221056@mail.shu.edu.tw>
2025-09-10 12:57:07 +0800

    114.09.10

---
+++ HelloWord/HelloWord/Form1.cs
@@ -0,0 +1,15 @@
+namespace HelloWord
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "順利完成第一個C#專案";
+        }
+    }
+}
