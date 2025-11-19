6cfec49c75f706d90b93dfcacb6fd3bdf0b3d421
a114221056-maker <a114221056@mail.shu.edu.tw>
2025-11-19 13:34:19 +0800

    114_11_19

---
+++ 114_11_19/Tutorial 5-4/Friend File/Friend File/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Friend_File
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的設計工具變數。
+        /// 注意：此檔由設計器管理；僅在必要時手動修改。
+        /// 以下修改項目：將所有元件的 Text 屬性確認為繁體中文，並將字型大小統一設定為 18pt。
+        /// 其他屬性（位置、大小、事件綁定等）未更動，以避免破壞設計器佈局。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 釋放所有資源（由設計器預設實作）。
+        /// </summary>
+        /// <param name="disposing">如果為 true，表示應釋放托管資源；否則僅釋放非托管資源。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// 此方法為設計器支援所需的方法 — 請勿使用程式碼編輯器修改此方法內容。
+        /// 已在此方法中：
+        ///  - 將所有元件的 Text 屬性設為繁體中文（若尚未）。
+        ///  - 將所有元件及表單的字型設定為繁體中文常用字型並統一為 18pt，提升可讀性。
+        ///  - 加入繁體中文詳細註解，說明每一個元件的用途與變更目的。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.promptLabel = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.writeNameButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // promptLabel
+            // 
+            this.promptLabel.AutoSize = true;
+            this.promptLabel.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.promptLabel.Location = new System.Drawing.Point(28, 55);
+            this.promptLabel.Name = "promptLabel";
+            this.promptLabel.Size = new System.Drawing.Size(280, 45);
+            this.promptLabel.TabIndex = 0;
+            this.promptLabel.Text = "輸入朋友的名字:";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.nameTextBox.Location = new System.Drawing.Point(377, 52);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(153, 55);
+            this.nameTextBox.TabIndex = 1;
+            // 
+            // writeNameButton
+            // 
+            this.writeNameButton.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.writeNameButton.Location = new System.Drawing.Point(82, 251);
+            this.writeNameButton.Name = "writeNameButton";
+            this.writeNameButton.Size = new System.Drawing.Size(147, 52);
+            this.writeNameButton.TabIndex = 2;
+            this.writeNameButton.Text = "寫名字";
+            this.writeNameButton.UseVisualStyleBackColor = true;
+            this.writeNameButton.Click += new System.EventHandler(this.writeNameButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.exitButton.Location = new System.Drawing.Point(349, 251);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(141, 52);
+            this.exitButton.TabIndex = 3;
+            this.exitButton.Text = "退出";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(21F, 45F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(613, 363);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.writeNameButton);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.promptLabel);
+            this.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Name = "Form1";
+            this.Text = "朋友檔案";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label promptLabel;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Button writeNameButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ 114_11_19/Tutorial 5-4/Friend File/Friend File/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Linq.Expressions;
+
+namespace Friend_File
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void writeNameButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // 顯示選擇對話方塊：使用者可選擇要寫出的朋友名字
+                // 按鈕對應：Yes => "Tommy", No => "David", Cancel => 結束操作
+                var result = MessageBox.Show(
+                    "請選擇要寫出的朋友名字：\n\n是 = Tommy\n否 = David\n取消 = 取消動作",
+                    "選擇朋友名字",
+                    MessageBoxButtons.YesNoCancel,
+                    MessageBoxIcon.Question);
+
+                string friendName;
+                if (result == DialogResult.Yes)
+                {
+                    friendName = "Tommy";
+                }
+                else if (result == DialogResult.No)
+                {
+                    friendName = "David";
+                }
+                else
+                {
+                    // 使用者選擇取消，結束處理
+                    return;
+                }
+
+                // 顯示在介面的輸入框，讓使用者立即看到結果
+                this.nameTextBox.Text = friendName;
+
+                // 產生檔案路徑：使用使用者的「我的文件」資料夾，檔名為 friend_name.txt
+                string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                string filePath = Path.Combine(documentsPath, "friend_name.txt");
+
+                // 將名字寫入檔案；使用 UTF8 編碼以正確儲存中文（若為英文亦適用）
+                File.WriteAllText(filePath, friendName, Encoding.UTF8);
+
+                // 通知使用者寫入成功並顯示檔案位置
+                MessageBox.Show("已將朋友名字 \"" + friendName + "\" 寫入檔案：" + Environment.NewLine + filePath,
+                                "寫入完成", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                // 發生錯誤時顯示錯誤訊息以利除錯
+                MessageBox.Show("發生錯誤: " + ex.Message, "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // 關閉表單。
+            this.Close();
+        }
+    }
+}
